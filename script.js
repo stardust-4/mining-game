5beab7cb4b5fd88ab0815cbba227971525524094
John <jboyle15@yahoo.com>
2022-10-04 14:27:09 -0500

    miner's animation works on keydown

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const container = document.querySelector('.container')
 const cellArray = []
+const cellIdCount = 0
+const miner = document.querySelector('.miner')
+const minerMining = document.querySelector('.minerMining')
 
-const createRow = (row) => {
-  for (i = 0; i < row; i++) {
+const createCell = (cell) => {
+  for (i = 0; i < cell; i++) {
     const div = document.createElement('div')
-    container.appendChild(div).className = 'row'
+    container.appendChild(div).className = 'cell'
     cellArray.push(div)
   }
-  console.log(cellArray)
-  cellArray[0].innerText = 'red'
+}
+const animateMiner = (event) => {
+  if (event.code == 'KeyM') {
+    miner.style.display = 'none'
+    minerMining.style.display = 'block'
+    const revertDisplay = () => {
+      miner.style.display = 'block'
+      minerMining.style.display = 'none'
+    }
+    setTimeout(revertDisplay, 125)
+  }
 }
 
-createRow(750)
+createCell(749)
+document.addEventListener('keydown', animateMiner)
+
+// const assignId = () => {
+//   cellArray.forEach((cell) => {
+//     cellIdCount += 1
+//   })
+//   console.log(cellIdCount)
+// }
+// console.log(cellArray)
+// cellArray[0].id = '0'
+
+// assignId()
