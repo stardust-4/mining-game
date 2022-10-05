a6d345da0b7c92005518a14ba68960bbd7be42eb
John <jboyle15@yahoo.com>
2022-10-05 10:15:05 -0500

    miner only moves to open cells

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const minerMining = document.querySelector('.minerMining')
 let keySCount = 1
 let keyDnACount = 1
 let currentLocation = 0
+let pointingUp = false
+let pointingRight = true
+let pointingDown = false
+let pointingLeft = false
 
 const createCell = (cell) => {
   for (i = 0; i < cell; i++) {
@@ -26,42 +30,82 @@ const animateMiner = (event) => {
     }
     setTimeout(revertDisplay, 137)
   } else if (event.code == 'KeyS') {
-    currentLocation += 15
+    pointingLeft = false
+    pointingRight = false
+    pointingDown = true
+    pointingUp = false
     miner.style.transform = 'rotate(90deg)'
     minerMining.style.transform = 'rotate(90deg)'
-    keySCount += 1
-    miner.style.gridRowStart = keySCount
-    minerMining.style.gridRowStart = keySCount
+    // keySCount += 1
+    // currentLocation +=15
+    // miner.style.gridRowStart = keySCount
+    // minerMining.style.gridRowStart = keySCount
   } else if (event.code == 'KeyD') {
-    currentLocation += 1
+    pointingLeft = false
+    pointingRight = true
+    pointingDown = false
+    pointingUp = false
     miner.style.transform = 'rotate(360deg)'
     minerMining.style.transform = 'rotate(360deg)'
-    keyDnACount += 1
-    miner.style.gridColumnStart = keyDnACount
-    minerMining.style.gridColumnStart = keyDnACount
+    // keyDnACount += 1
+    // currentLocation += 1
+    // miner.style.gridColumnStart = keyDnACount
+    // minerMining.style.gridColumnStart = keyDnACount
   } else if (event.code == 'KeyA') {
-    currentLocation -= 1
+    pointingLeft = true
+    pointingRight = false
+    pointingDown = false
+    pointingUp = false
     miner.style.transform = 'scaleX(-1)'
     minerMining.style.transform = 'scaleX(-1)'
-    keyDnACount -= 1
-    miner.style.gridColumnStart = keyDnACount
-    minerMining.style.gridColumnStart = keyDnACount
+    // keyDnACount -= 1
+    // currentLocation -= 1
+    // miner.style.gridColumnStart = keyDnACount
+    // minerMining.style.gridColumnStart = keyDnACount
   }
   //allows miner to go up
   else if (event.code == 'KeyW') {
-    currentLocation -= 15
+    pointingLeft = false
+    pointingRight = false
+    pointingDown = false
+    pointingUp = true
     miner.style.transform = 'rotate(-90deg)'
     minerMining.style.transform = 'rotate(-90deg)'
-    keySCount -= 1
-    miner.style.gridRowStart = keySCount
-    minerMining.style.gridRowStart = keySCount
+    // keySCount -= 1
+    // currentLocation -= 15
+    // miner.style.gridRowStart = keySCount
+    // minerMining.style.gridRowStart = keySCount
   }
 }
 const mining = (event) => {
-  if (event.code == 'KeyM') {
-    document.getElementById(`${currentLocation}`).style.backgroundColor = 'red'
+  if (event.code == 'KeyM' && pointingRight === true) {
+    document.getElementById(`${currentLocation}`).style.backgroundColor =
+      '#00f5d4'
+    document.getElementById(`${currentLocation}`).classList.add('minedCell')
+  } else if (event.code == 'KeyM' && pointingLeft === true) {
+    document.getElementById(`${currentLocation}` - 1).style.backgroundColor =
+      '#00f5d4'
+  } else if (event.code == 'KeyM' && pointingDown === true) {
+    document.getElementById(`${currentLocation}` + 15).style.backgroundColor =
+      '#00f5d4'
+  } else if (event.code == 'KeyM' && pointingUp === true) {
+    document.getElementById(`${currentLocation}` - 15).style.backgroundColor =
+      '#00f5d4'
   }
 }
+const movingMiner = (event) => {
+  if (
+    event.code === 'KeyD' &&
+    document.getElementById(`${currentLocation}`).className === 'cell minedCell'
+  ) {
+    console.log('hi')
+    keyDnACount += 1
+    currentLocation += 1
+    miner.style.gridColumnStart = keyDnACount
+    minerMining.style.gridColumnStart = keyDnACount
+  }
+}
+document.addEventListener('keydown', movingMiner)
 
 document.addEventListener('keydown', mining)
 createCell(749)
